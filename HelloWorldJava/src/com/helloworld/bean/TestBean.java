8efb8b3e15c2a573b4ea4fc00edf718b9354b947
vreddytangella <96090436+vreddytangella@users.noreply.github.com>
2021-12-17 22:12:57 +0530

    bean changes to master

---
+++ HelloWorldJava/src/com/helloworld/bean/TestBean.java
@@ -0,0 +1,14 @@
+package com.helloworld.bean;
+
+public class TestBean {
+
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- HelloWorldJava/src/com/helloworld/bean/UserBean.java
+++ HelloWorldJava/src/com/helloworld/bean/UserBean.java
@@ -9,4 +9,22 @@ package com.helloworld.bean;
  */
 public class UserBean {
 
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
