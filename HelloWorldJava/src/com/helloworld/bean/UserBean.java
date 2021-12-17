0207e81f7172f665cbe9650ee7c4134acb8c0d9b
vreddytangella <96090436+vreddytangella@users.noreply.github.com>
2021-12-17 22:14:16 +0530

    commiting uaserbean changes to br1

--- HelloWorldJava/src/com/helloworld/bean/UserBean.java
+++ HelloWorldJava/src/com/helloworld/bean/UserBean.java
@@ -9,4 +9,13 @@ package com.helloworld.bean;
  */
 public class UserBean {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- HelloWorldJava/src/com/helloworld/main/HelloWorldMain.java
+++ HelloWorldJava/src/com/helloworld/main/HelloWorldMain.java
@@ -12,7 +12,7 @@ public class HelloWorldMain {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String []args) {
 		// TODO Auto-generated method stub
 		System.out.println("first commit to branch1");
 
